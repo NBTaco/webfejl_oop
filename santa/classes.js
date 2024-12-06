f8dec1daf61e57675e9883e80f744340e7db530b
Meszaros Nandor <meszaros.nandor@BOLYAI.LOCAL>
2024-12-06 08:43:01 +0100

    Factory, Commpanion

--- santa/classes.js
+++ santa/classes.js
@@ -1,8 +1,25 @@
 
 class Factory{
     // TODO 1, 2, 3, 4, 9, 10
-   }
+    constructor()
+    {
+        this.manolist = []
+    }
+    addMano(mano)
+    {
+        
+    }
+}
    
    class Companion{
-    // TODO 5
-   }
+    // TODO 
+    constructor(id, keresztnev, vezeteknev, reszleg)
+        {
+            this.id = id
+            this.keresztnev = keresztnev
+            this.vezeteknev = vezeteknev 
+            this.reszleg = reszleg
+            this.products = []
+        }
+    
+}
