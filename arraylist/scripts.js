c2dc41d9fec50debbac291ee5177bf1705ddc138
Meszaros Nandor <meszaros.nandor@bolyai.local>
2025-01-17 10:06:13 +0100

    tryhard programming

---
+++ arraylist/scripts.js
@@ -0,0 +1,64 @@
+class ArrayList {
+    /**
+     * @type {Number}
+     */
+    #aktualishossz
+
+    /**
+     * 
+     */
+    #state
+
+    get Count() {
+        return this.#aktualishossz
+    }
+
+    constructor(){
+        this.#aktualishossz = 0
+        this.#state = {}
+    }
+
+    Add(element) {
+        const index = this.#aktualishossz
+        this.#state[index] = element
+
+        Object.defineProperty(this, index, {
+            get: function(){
+                return this.#state[index]
+            },
+            set: function(value){
+                this.#state[index] = value
+            }
+        })
+
+        this.#aktualishossz++
+    }
+
+    Clear(){
+        this.#aktualishossz = 0
+        this.#state = {}
+    }
+}
+
+const alma = {}
+Object.defineProperty(alma, 'nev', {
+    value: 'Ferenc',
+    wriable: true
+})
+
+alma.nev = 'niagara'
+console.log(alma)
+
+
+const lista = new ArrayList()
+lista.Add('asd1')
+console.log(lista)
+
+lista.Add('asd2')
+console.log(lista)
+
+lista.Add({new: 'asd3'})
+console.log(lista)
+
+console.log(lista[0])
+
