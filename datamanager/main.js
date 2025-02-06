eafef37468a7b2331c19ec697be301bbe7fdd400
NBTaco <menandor119@gmail.com>
2025-02-06 11:12:24 +0100

    Házi: renderTable, #tbody, input mezők, Number()

--- datamanager/main.js
+++ datamanager/main.js
@@ -82,32 +82,87 @@ class DataTable{
         const thead = document.createElement('thead')
         table.appendChild(thead)
 
-        const tbody = document.createElement('tbody')
-        table.appendChild(tbody)
+        this.#tbody = document.createElement('tbody')
+        table.appendChild(this.#tbody)
 
         datamanager.setupdatecallback(persons => {
-            tbody.innerHTML = ""
-
-            for(let i = 0; i < persons.length; i++){
-                const tr = document.createElement('tr')
-                tbody.appendChild(tr)
-
-                const td = document.createElement('td')
-                td.innerHTML = persons[i].nev
-                tr.appendChild(td)
-
-                const td2 = document.createElement('td')
-                td2.innerHTML = persons[i].eletkor
-                tr.appendChild(td2)
-            }
+            this.#renderTable(persons)
         })
+    }
+
+    /**
+         * 
+         * @param {HTMLTableSectionElement}
+         */
+    #tbody
+
+    /**
+    * 
+    * @param {Person[]} persons 
+    */
+    #renderTable(persons){
+        this.#tbody.innerHTML = ''
+
+        for (let i = 0; i < persons.length; i++){
+            const tr = document.createElement('tr')
+            this.#tbody.appendChild(tr)
+
+            const td = document.createElement('td')
+            td.innerHTML = persons[i].nev
+            tr.appendChild(td)
+
+            const td2 = document.createElement('td')
+            td2.innerHTML = persons[i].eletkor
+            tr.appendChild(td2)
+        }
     }
 }
 
 const dm = new Datamanager([
     {nev: 'Józsi', eletkor: 17},
     {nev: 'Teri', eletkor: 16},
-    {nev: 'Gábor', eletkor: 17}
+    {nev: 'Gábor', eletkor: 17},
+    {nev: 'Pisti', eletkor: 17},
+    {nev: 'Jani', eletkor: 23},
+    {nev: 'Teri', eletkor: 17},
+    {nev: 'Ádám', eletkor: 17},
+    {nev: 'Zoli', eletkor: 21},
+    {nev: 'Peti', eletkor: 20},
+    {nev: 'Nándi', eletkor: 20},
+    {nev: 'Matyi', eletkor: 19},
+
 ])
 
 const table = new DataTable(dm)
+
+const label1 = document.createElement('label')
+label1.innerHTML = 'Név: '
+document.body.appendChild(label1)
+
+const input1 = document.createElement('input')
+document.body.appendChild(input1)
+
+const br1 = document.createElement('br')
+document.body.appendChild(br1)
+
+const label2 = document.createElement('label')
+label2.innerHTML = 'Életkor: '
+document.body.appendChild(label2)
+
+const input2 = document.createElement('input')
+document.body.appendChild(input2)
+
+const br2 = document.createElement('br')
+document.body.appendChild(br2)
+
+input1.addEventListener('input', function(e){
+    e.preventDefault()
+
+    dm.filtername(e.target.value)
+})
+
+input2.addEventListener('input', function(e){
+    e.preventDefault()
+
+    dm.filterage(Number(e.target.value))
+})
